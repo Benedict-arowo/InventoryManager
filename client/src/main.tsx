f9f2df8c18499360d2bf0f0b4fd59cd94e5526ac
Benedict <benedict.arowo@gmail.com>
2024-02-17 21:43:37 +0300

    Installed PrimeReact

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { PrimeReactProvider } from "primereact/api";
+import "primereact/resources/primereact.min.css";
+import "./index.css";
+import "primereact/resources/themes/bootstrap4-light-blue/theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<PrimeReactProvider value={{ unstyled: false, pt: {} }}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</PrimeReactProvider>
 	</React.StrictMode>
 );
