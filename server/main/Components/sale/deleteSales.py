003d9854e0652d9a01ffeb8891fea0a3e037dc07
Benedict <benedict.arowo@gmail.com>
2024-02-17 22:05:17 +0300

    Fixes, and file cleanup

--- server/main/Components/sale/deleteSales.py
+++ server/main/Components/sale/deleteSales.py
@@ -5,21 +5,9 @@ from ...Components.CustomError import CustomException
 
 
 def deleteSales(id):
-    # if not request.user.is_authenticated:
-    #     return Response(
-    #         {"error": "You must be authenticated to access this route."},
-    #         status=status.HTTP_401_UNAUTHORIZED,
-    #     )
-
-    # if not request.user.is_staff:
-    #     return Response(
-    #         {"error": "You do not have permission to access this route."},
-    #         status=status.HTTP_401_UNAUTHORIZED,
-    #     )
-
     try:
         sale = models.Sale.objects.get(id=id)
-        item = models.Stock.objects.get(name=sale.name)
+        item = models.Stock.objects.get(name=sale.item.name)
 
         # Add's the item back to the stock
         item.quantity = item.quantity + sale.quantity
@@ -31,6 +19,6 @@ def deleteSales(id):
 
         return {"data": {"success": True}, "status": status.HTTP_204_NO_CONTENT}
     except models.Sale.DoesNotExist:
-        raise CustomException("Quantity not found.", status.HTTP_404_NOT_FOUND)
+        raise CustomException("Sale not found.", status.HTTP_404_NOT_FOUND)
     except Exception as e:
         raise CustomException(str(e), code=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- server/main/urls.py
+++ server/main/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import sale, stock, expences
 
 urlpatterns = [
-    path('sales/', sale.sales, name="getSales"),
-    path('sale/<uuid:id>', sale.sale, name="sale"),
-    path('stock', stock.stock, name="stock"),
-    path('expences', expences.expences, name="expences"),
-    path('expence/<uuid:id>', expences.expence, name="expence"),
+    path("sales/", sale.sales, name="getSales"),
+    path("sale/<int:id>", sale.sale, name="sale"),
+    path("stock", stock.stock, name="stock"),
+    path("expences", expences.expences, name="expences"),
+    path("expence/<uuid:id>", expences.expence, name="expence"),
 ]
