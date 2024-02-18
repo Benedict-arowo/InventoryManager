9ae5a01eb83f592696eb2d115a04fa61cb2a4f60
Benedict <benedict.arowo@gmail.com>
2024-02-18 09:40:04 +0300

    fix: Create sales

--- client/src/pages/Sales.tsx
+++ client/src/pages/Sales.tsx
@@ -12,6 +12,12 @@ import { Calendar } from "primereact/calendar";
 import { CascadeSelect } from "primereact/cascadeselect";
 import { AutoComplete } from "primereact/autocomplete";
 import dateFormat from "dateformat";
+import { Dialog } from "primereact/dialog";
+import { Fab } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import { Dropdown } from "primereact/dropdown";
+import { InputSwitch } from "primereact/inputswitch";
+import { InputNumber } from "primereact/inputnumber";
 
 type Item = {
   id: string;
@@ -45,6 +51,36 @@ type Sale = {
 
 const Sales = () => {
   const [salesItems, setSalesItems] = useState<Sale[]>([]);
+  const [stock, setStock] = useState<Item[]>([]);
+  const [newItemState, setNewItemState] = useState({
+    name: "",
+    quantity: 0,
+    price: 0,
+    amount_paid: 0,
+    payment_method: "",
+    minimum_price: 0,
+  });
+  const [openNewSaleModal, setOpenNewSaleModal] = useState(false);
+
+  const fetchStock = async () => {
+    // let itemData;
+
+    // fetch(`${SERVER_URL}/api/stock`, { method: "GET" })
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     itemData = data;
+    //   })
+    //   .catch((err) => console.log(err));
+    const response = await fetch(`${SERVER_URL}/api/stock`, { method: "GET" });
+    return await response.json();
+  };
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await fetchStock();
+      setStock(data);
+    })();
+  }, []);
 
   const totalBodyTemplate = (item: Sale) => {
     return <p>{item.price * item.quantity}</p>;
@@ -159,6 +195,61 @@ const Sales = () => {
   ];
   const home = { icon: "pi pi-home", url: "." };
 
+  const paymentMethods = [
+    { name: "CASH", code: "cash" },
+    { name: "TRANSFER", code: "transfer" },
+    { name: "CARD", code: "card" },
+    { name: "NONE", code: "none" },
+  ];
+
+  const resetNewItemState = () => {
+    setNewItemState(() => {
+      return {
+        name: "",
+        quantity: 0,
+        price: 0,
+        amount_paid: 0,
+        payment_method: "",
+      };
+    });
+  };
+
+  const updateItemBasedOnItemName = (e: string) => {
+    const item = stock.find((item) => item.name === e);
+    setNewItemState((curr) => {
+      return {
+        ...curr,
+        price: item ? item.price_per_unit : 0,
+        minimum_price: item ? item.price_per_unit : 0,
+      };
+    });
+  };
+
+  const createSale = () => {
+    if (!newItemState.name) return alert("Please select an item to sell");
+    if (newItemState.quantity < 1)
+      return alert("Quantity must be greater than 0");
+    if (newItemState.price < 1) return alert("Price must be greater than 0");
+    if (!newItemState.payment_method)
+      return alert("Please select a payment method");
+
+    fetch(`${SERVER_URL}/api/sales/`, {
+      method: "POST",
+      body: JSON.stringify(newItemState),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) return console.error(data.error);
+        setSalesItems((curr) => [...curr, data.data]);
+        resetNewItemState();
+        setOpenNewSaleModal(false);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="w-full relative text-zinc-700">
       <BreadCrumb className="bg-transparent mt-1" model={items} home={home} />
@@ -262,7 +353,7 @@ const Sales = () => {
             <Column
               field="id"
               header="#"
-              style={{ width: "32px", fontSize: ".8rem" }}
+              style={{ width: "32px", fontSize: ".9rem" }}
             ></Column>
             <Column
               body={(item) => <p className="capitalize">{item.item.name}</p>}
@@ -292,6 +383,133 @@ const Sales = () => {
           </DataTable>
         </div>
       </div>
+
+      <Dialog
+        header="Create a new sale..."
+        visible={openNewSaleModal}
+        draggable={false}
+        dismissableMask
+        style={{ width: "50vw" }}
+        onHide={() => setOpenNewSaleModal(false)}
+      >
+        <div className="flex flex-col gap-1">
+          <label htmlFor="name">Name</label>
+          <AutoComplete
+            id="name"
+            aria-describedby="item-name"
+            suggestions={stock.map((item) => item.name)}
+            completeMethod={async () => {
+              const { data } = await fetchStock();
+              setStock(data);
+            }}
+            value={newItemState.name}
+            onChange={(e) => {
+              setNewItemState({ ...newItemState, name: e.target.value });
+            }}
+            onSelect={(e) => {
+              updateItemBasedOnItemName(e.value);
+            }}
+            forceSelection
+          />
+        </div>
+        <div className="flex flex-row gap-4">
+          <div className="flex flex-col gap-1 flex-1">
+            <label htmlFor="quantity">Quantity</label>
+            <InputText
+              value={String(newItemState.quantity)}
+              onChange={(e) =>
+                setNewItemState({
+                  ...newItemState,
+                  quantity: Number(e.target.value),
+                })
+              }
+              keyfilter="int"
+              id="quantity"
+              aria-describedby="item-quantity"
+            />
+          </div>
+          <div className="flex flex-col gap-1 flex-1">
+            <label htmlFor="price">Price</label>
+            <InputNumber
+              id="price"
+              aria-describedby="price-name"
+              value={newItemState.price}
+              onValueChange={(e) =>
+                setNewItemState({
+                  ...newItemState,
+                  price: e.target.value ? e.target.value : 0,
+                })
+              }
+              min={newItemState.minimum_price}
+              useGrouping={false}
+            />
+          </div>
+        </div>
+        <div className="flex flex-row gap-4">
+          <div className="flex flex-col gap-1 flex-1">
+            <label htmlFor="amount_paid">Amount Paid</label>
+            <InputText
+              value={String(newItemState.amount_paid)}
+              onChange={(e) =>
+                setNewItemState({
+                  ...newItemState,
+                  amount_paid: Number(e.target.value),
+                })
+              }
+              keyfilter="int"
+              id="amount_paid"
+              aria-describedby="amount-paid"
+            />
+          </div>
+          <div className="flex flex-col gap-1 flex-1">
+            <label htmlFor="payment_method">Payment Method</label>
+            <Dropdown
+              value={newItemState.payment_method}
+              onChange={(e) =>
+                setNewItemState({ ...newItemState, payment_method: e.value })
+              }
+              options={paymentMethods}
+              id="payment_method"
+              aria-describedby="payment-method"
+              optionLabel="name"
+              placeholder="Select a payment method"
+              className="w-full md:w-14rem"
+            />
+          </div>
+        </div>
+        <div className="flex flex-row gap-4 justify-center mt-4">
+          <Button
+            label="Create"
+            severity="success"
+            className="w-fit h-fit px-6"
+            onClick={createSale}
+          />
+          <Button
+            label="Cancel"
+            severity="danger"
+            className="w-fit h-fit px-6"
+            onClick={() => {
+              resetNewItemState();
+              setOpenNewSaleModal(false);
+            }}
+          />
+        </div>
+      </Dialog>
+
+      {/* Add Icon */}
+      <Fab
+        color="primary"
+        aria-label="add"
+        size="medium"
+        onClick={() => setOpenNewSaleModal(() => true)}
+        sx={{
+          position: "absolute",
+          bottom: 16,
+          right: 32,
+        }}
+      >
+        <AddIcon />
+      </Fab>
     </div>
   );
 };
