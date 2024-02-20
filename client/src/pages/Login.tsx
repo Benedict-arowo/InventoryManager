30e84fe3b52655d9df2beea10ebcdd92f556dafc
Benedict <benedict.arowo@gmail.com>
2024-02-20 11:15:52 +0300

    fix: Loader, error messages in login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -3,16 +3,25 @@ import { InputText } from "primereact/inputtext";
 import { Password } from "primereact/password";
 import { useState } from "react";
 import { SERVER_URL } from "../utils";
+import { useNavigate } from "react-router-dom";
+import { Message } from "primereact/message";
+import { ProgressSpinner } from "primereact/progressspinner";
 
 const Login = () => {
   const [details, setDetails] = useState({
     username: "",
     password: "",
   });
+  const [err, setErr] = useState<null | string>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
+  const Navigate = useNavigate();
   const loginUser = () => {
-    if (!details.username) throw new Error("Please enter a username");
-    if (!details.password) throw new Error("Please enter a password");
+    if (!details.username) return setErr("Username must be provided");
+    if (!details.password) return setErr("Please enter a password");
+
+    setErr(null);
+    setIsLoading(true);
 
     fetch(`${SERVER_URL}/api-auth/token/`, {
       body: JSON.stringify(details),
@@ -21,17 +30,26 @@ const Login = () => {
         "Content-Type": "application/json",
       },
     })
-      .then((response) => response.json())
+      .then(async (response) => {
+        if (!response.ok) throw new Error("Error while trying to log user in.");
+        return response.json();
+      })
       .then((data) => {
         localStorage.setItem("access-token", JSON.stringify(data.access));
         localStorage.setItem("refresh-token", JSON.stringify(data.refresh));
-        alert("Logged In Successfully");
+        Navigate("/dashboard");
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err);
+        return setErr(err.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
 
   return (
-    <div className="w-full h-screen grid place-content-center overflow-hidden bg-slate-700">
+    <div className="w-full h-screen grid place-content-center gap-2 overflow-hidden bg-slate-700">
       <section className="w-fit px-4 pt-2 pb-6 bg-neutral-200">
         <div className="flex flex-col gap-2 mt-4">
           <InputText
@@ -53,8 +71,12 @@ const Login = () => {
           />
 
           <Button onClick={loginUser} className="mt-2" label="Login" />
+          {isLoading && (
+            <ProgressSpinner style={{ width: "30px", height: "30px" }} />
+          )}
         </div>
       </section>
+      {err && <Message severity="error" text={err} />}
     </div>
   );
 };
