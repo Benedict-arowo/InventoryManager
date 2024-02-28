5f676de35d835df89c4617db459f60d10699ca9f
Benedict <benedict.arowo@gmail.com>
2024-02-28 12:21:14 +0300

    Refactor UseFetch function signature and error handling

--- client/src/UseFetch.tsx
+++ client/src/UseFetch.tsx
@@ -1,46 +1,52 @@
 import { SERVER_URL } from "./utils";
 
-type useFetchProps = {
+interface FetchParams {
   url: string;
   options: {
-    method: "GET" | "PATCH" | "DELETE" | "POST" | "PUT";
-    body?: object;
+    useServerUrl?: boolean;
+    method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+    body?: object | undefined;
     returnResponse?: boolean;
-    useServerUrl: boolean;
-    useAuth: boolean;
+    handleError?: boolean;
   };
-};
+}
 
-// type returnObj = {
-//   data?: object;
-//   error?: object;
-// };
-
-const UseFetch = async ({ url, options }: useFetchProps) => {
+const UseFetch = async ({ url, options }: FetchParams) => {
   // Attempts to get access token from localhost
   const stringifiedToken = localStorage.getItem("access-token");
   let accessToken;
   if (stringifiedToken) accessToken = JSON.parse(stringifiedToken) as string;
 
-  if (options.useAuth && !accessToken) throw new Error("Missing access token.");
+  try {
+    const response = await fetch(
+      options.useServerUrl ? `${SERVER_URL}/${url}` : url,
+      {
+        method: options?.method || "GET",
+        // credentials: "omit",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: options?.body ? JSON.stringify(options.body) : null,
+      }
+    );
 
-  const response = await fetch(
-    `${options.useServerUrl ? `${SERVER_URL}/` : ""}${url}`,
-    {
-      method: options.method ? options.method : "GET",
-      body: options.body ? JSON.stringify(options.body) : undefined,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${accessToken}`,
-      },
+    const data = await response.json();
+
+    if (!response.ok && options.handleError) {
+      if (data.message) throw new Error(data.message);
+      else
+        throw new Error(
+          "Internal Server Error.... Try contacting an administrator."
+        );
     }
-  );
-  const data = await response.json();
 
-  if (options.returnResponse) {
-    return { data, response };
-  } else {
-    return { data };
+    if (options.returnResponse) return { data, response };
+    else return data;
+  } catch (error) {
+    // Handle any errors that occurred during the fetch
+    console.error("Fetch error:", error);
+    throw error; // Propagate the error to the calling component
   }
 };
 
